4d52cde8c9f27888ef3c1f5e5e46d553918c4773
Elijah <ilyas@ViciousXerra.localdomain>
2026-03-08 21:57:25 +0300

    modified development ci pipeline

--- .github/workflows/semver-compare.sh
+++ .github/workflows/semver-compare.sh
@@ -2,9 +2,9 @@
 # $1 - base version
 # $2 - head version
 if dpkg --compare-versions "$1" "gt" "$2"; then
-    echo "-1"
+    echo -1
 elif dpkg --compare-versions "$1" "lt" "$2"; then
-    echo "1"
+    echo 1
 else
-    echo "0"
+    echo 0
 fi
